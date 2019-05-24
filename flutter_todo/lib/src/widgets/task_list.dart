a81efb706e02d6e41b4098435df3b41c626a6c62
yasar budulgan <yb.budulgan@gmail.com>
2019-05-25 00:47:36 +0300

    Add dismiss function by swiping cards

--- flutter_todo/lib/src/widgets/task_list.dart
+++ flutter_todo/lib/src/widgets/task_list.dart
@@ -14,9 +14,9 @@ class _TaskListState extends State<TaskList> {
   @override
   void initState() {
     List<Task> myList = [
-      Task('task 1', TaskStatus.postponed, 1),
-      Task('task 2', TaskStatus.postponed, 2),
-      Task('task 3', TaskStatus.postponed, 3),
+      Task('Complete 10 push ups', TaskStatus.postponed, 1),
+      Task('Interview schedule for Mike', TaskStatus.postponed, 2),
+      Task('Meeting', TaskStatus.postponed, 3),
     ];
 
     setState(() {
@@ -27,17 +27,76 @@ class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Container(
-          child: ListView.builder(
-            itemCount: _taskList.length,
-            itemBuilder: (context, position) {
-              return TaskListItem(_taskList[position]);
+      child: ListView.separated(
+        itemCount: _taskList.length,
+        separatorBuilder: (context, position) {
+          return SizedBox(
+            height: 15,
+          );
+        },
+        itemBuilder: (context, position) {
+          final task = _taskList[position];
+          return Dismissible(
+            key: Key(task.title),
+            onDismissed: (DismissDirection dir) {
+              setState(() => _taskList.removeAt(position));
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(dir == DismissDirection.startToEnd ? "Item removed" : "Item Liked"),
+                  action: SnackBarAction(
+                      label: "UNDO",
+                      onPressed: () {
+                        setState(() => _taskList.insert(position, task));
+                      }),
+                ),
+              );
             },
-          ),
-        ),
+            background: _LeftToRight(),
+            secondaryBackground: _RightToLeft(),
+            child: TaskListItem(_taskList[position]),
+          );
+        },
       ),
+    );
+  }
+}
+
+class _LeftToRight extends StatelessWidget {
+  const _LeftToRight({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 20),
+      child: Icon(
+        Icons.check,
+        color: Colors.white,
+        size: 32,
+      ),
+      color: Colors.green,
+      alignment: Alignment.centerLeft,
+    );
+  }
+}
+
+class _RightToLeft extends StatelessWidget {
+  const _RightToLeft({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(right: 20),
+      child: Icon(
+        Icons.alarm,
+        color: Colors.white,
+        size: 32,
+      ),
+      color: Colors.amber.shade700,
+      alignment: Alignment.centerRight,
     );
   }
 }
--- flutter_todo/lib/src/widgets/task_list_item.dart
+++ flutter_todo/lib/src/widgets/task_list_item.dart
@@ -9,8 +9,35 @@ class TaskListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 8,
-      child: Text(task.title),
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      elevation: 2,
+      child: Container(
+        padding: EdgeInsets.fromLTRB(15, 15, 25, 15),
+        child: Row(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  task.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Text(
+                  'Due Fri Agu 8',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
