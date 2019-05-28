237b8ecf113fc0af3e8aee529ff1c10ea343f1c0
yasar budulgan <yb.budulgan@gmail.com>
2019-05-28 23:37:36 +0300

    Randomize initial task due dates

--- flutter_todo/lib/src/models/task.dart
+++ flutter_todo/lib/src/models/task.dart
@@ -1,9 +1,22 @@
 enum TaskStatus { postponed, done }
 
-class Task {
+class Task implements Comparable<Task> {
   final String title;
   final TaskStatus status;
-  final int dueDateTimestamp;
+  final DateTime dueDate;
 
-  Task(this.title, this.status, this.dueDateTimestamp);
+  Task(this.title, this.status, this.dueDate);
+
+  Task copyWith({String title, TaskStatus status, DateTime dueDate}) {
+    return new Task(
+      title ?? this.title,
+      status ?? this.status,
+      dueDate ?? this.dueDate,
+    );
+  }
+
+  @override
+  int compareTo(Task otherTask) {
+    return this.dueDate.compareTo(otherTask.dueDate);
+  }
 }
--- flutter_todo/lib/src/provider/task_list_provider.dart
+++ flutter_todo/lib/src/provider/task_list_provider.dart
@@ -1,22 +1,39 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter_todo/src/models/task.dart';
 
 class TaskListProvider extends ChangeNotifier {
+  static final int MIN_TIMESTAMP = 1564691473;
+  static final int MAX_TIMESTAMP = 1627824673;
+  static final int DIFF = MAX_TIMESTAMP - MIN_TIMESTAMP;
+
   List<Task> taskList;
 
   TaskListProvider() {
+    var rng = new Random(123456789);
+
+    List<DateTime> randomDates = new List.generate(
+      9,
+      (_) => new DateTime.fromMillisecondsSinceEpoch((rng.nextInt(DIFF) + MIN_TIMESTAMP) * 1000),
+    );
+
+    randomDates.shuffle();
+
     taskList = [
-      Task('Complete 10 push ups', TaskStatus.postponed, 1),
-      Task('Interview schedule for Mike', TaskStatus.postponed, 2),
-      Task('Meeting', TaskStatus.postponed, 3),
-      Task('Complete 10 push ups', TaskStatus.postponed, 1),
-      Task('Interview schedule for Mike', TaskStatus.postponed, 2),
-      Task('Meeting', TaskStatus.postponed, 3),
-      Task('Complete 10 push ups', TaskStatus.postponed, 1),
-      Task('Interview schedule for Mike', TaskStatus.postponed, 2),
-      Task('Meeting', TaskStatus.postponed, 3),
+      Task('Complete 10 push ups', TaskStatus.postponed, randomDates[0]),
+      Task('Interview schedule for Mike', TaskStatus.postponed, randomDates[1]),
+      Task('Meeting', TaskStatus.postponed, randomDates[2]),
+      Task('Complete 10 push ups', TaskStatus.postponed, randomDates[3]),
+      Task('Interview schedule for Mike', TaskStatus.postponed, randomDates[4]),
+      Task('Meeting', TaskStatus.postponed, randomDates[5]),
+      Task('Complete 10 push ups', TaskStatus.postponed, randomDates[6]),
+      Task('Interview schedule for Mike', TaskStatus.postponed, randomDates[7]),
+      Task('Meeting', TaskStatus.postponed, randomDates[8]),
     ];
+
+    taskList.sort();
   }
 
   Task get(int position) {
@@ -28,11 +45,8 @@ class TaskListProvider extends ChangeNotifier {
   }
 
   void add(Task task, {int position = -1}) {
-    if (position == -1) {
-      taskList.add(task);
-    } else {
-      taskList.insert(position, task);
-    }
+    taskList.add(task);
+    taskList.sort();
 
     notifyListeners();
   }
@@ -43,6 +57,9 @@ class TaskListProvider extends ChangeNotifier {
   }
 
   void postpone(DateTime newDate, int position) {
-    Task task = taskList[position];
+    Task newTask = taskList[position];
+
+    removeAt(position);
+    add(newTask, position: position);
   }
 }
