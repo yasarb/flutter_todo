3001ae408286bcbcf6e8c39720bdff35f1d17b66
yasar budulgan <yb.budulgan@gmail.com>
2019-05-28 23:00:56 +0300

    Add provider package dependency

--- flutter_todo/lib/src/app.dart
+++ flutter_todo/lib/src/app.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_todo/src/provider/task_list_provider.dart';
 import 'package:flutter_todo/src/screens/home_page.dart';
 
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      theme: ThemeData(
-        fontFamily: 'Nunito',
-        primarySwatch: Colors.pink,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0x778E9EAB),
-                Color(0xFFEEF2F3),
-              ],
+    return ChangeNotifierProvider<TaskListProvider>(
+      builder: (_) => TaskListProvider(),
+      child: MaterialApp(
+        title: 'Todo App',
+        theme: ThemeData(
+          fontFamily: 'Nunito',
+          primarySwatch: Colors.pink,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0x778E9EAB),
+                  Color(0xFFEEF2F3),
+                ],
+              ),
             ),
+            child: HomePage(),
           ),
-          child: HomePage(),
         ),
       ),
     );
